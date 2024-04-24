43c47f2081d89d8bb9e32729375b28aaf92f23cf
raj-malasoni <raj.malasoni@impressico.com>
2024-04-23 21:47:50 -0400

    adding

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from github import Github
 from datetime import datetime, timedelta
 import requests
 print("hello from main pyhon code")
+print (f"value = {os.environ["GITHUB_TOKEN"]}")
 
 try:
     # Get GitHub token from environment variable
